f5e9d5a6099848689449363a3b34d250769ca341
砒霜 <qiuxutao_vip@163.com>
2022-08-28 17:27:07 +0800

    2

--- src/main/java/com/spring/using/algorithm/两数相加.java
+++ src/main/java/com/spring/using/algorithm/两数相加.java
@@ -32,38 +32,40 @@ package com.spring.using.algorithm;
  * 题目数据保证列表表示的数字不含前导零
  *
  */
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
 public class 两数相加 {
     public static void main(String[] args) {
 
     }
-}
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode root = new ListNode(0);
-        ListNode cursor = root;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0) {
-            int l1Val = l1 != null ? l1.val : 0;
-            int l2Val = l2 != null ? l2.val : 0;
-            int sumVal = l1Val + l2Val + carry;
-            carry = sumVal / 10;
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode root = new ListNode(0);
+            ListNode cursor = root;
+            int carry = 0;
+            while(l1 != null || l2 != null || carry != 0) {
+                int l1Val = l1 != null ? l1.val : 0;
+                int l2Val = l2 != null ? l2.val : 0;
+                int sumVal = l1Val + l2Val + carry;
+                carry = sumVal / 10;
 
-            ListNode sumNode = new ListNode(sumVal % 10);
-            cursor.next = sumNode;
-            cursor = sumNode;
+                ListNode sumNode = new ListNode(sumVal % 10);
+                cursor.next = sumNode;
+                cursor = sumNode;
 
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
+                if(l1 != null) l1 = l1.next;
+                if(l2 != null) l2 = l2.next;
+            }
+            return root.next;
         }
-        return root.next;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
+
+
 
 
